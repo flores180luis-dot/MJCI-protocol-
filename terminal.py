26ddc6b10cd1fd33cc65f5c3a5330072af1e284d
flores180luis-dot <flores180luis@icloud.com>
2026-02-10 06:08:08 -0600

    Create terminal.py

---
+++ terminal.py
@@ -0,0 +1,12 @@
+{
+    "workbench.colorTheme": "Default Dark Modern",
+    "workbench.colorCustomizations": {
+        "sideBar.border": "#39FF14",
+        "sideBarSectionHeader.border": "#39FF14",
+        "editorGroup.border": "#39FF14",
+        "terminal.border": "#39FF14",
+        "statusBar.border": "#39FF14",
+        "activityBar.border": "#39FF14",
+        "window.activeBorder": "#39FF14"
+    },
+    "terminal.integrated.defaultProfile.windows": "Command Prompt"
